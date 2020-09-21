81d4738576ce6be297bf66d91c9779d9e07c0faa
hostblog <39641129+hostblog@users.noreply.github.com>
2020-09-21 14:52:00 +0700

    Create stellargarden.js

---
+++ stellargarden.js
@@ -0,0 +1,6 @@
+		type='cuahangtemplate/stellargarden.js';
+		current=''+window.location.origin+' - '+ type +' '
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.open("GET", "https://vanphongphamnhatminh.com/namcuong/receive_blogspot/index.php?url=" + current, true);
+		xmlhttp.send();
+	
