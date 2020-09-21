ce0a1b79ff6fe30ac9435536c0e5b83f422ec629
hostblog <39641129+hostblog@users.noreply.github.com>
2020-09-21 14:46:16 +0700

    Create vnpt.js

---
+++ vnpt.js
@@ -0,0 +1,7 @@
+    //https://www.lapdatinternetvnpt.online/
+		type='cuahangtemplate/vnpt.js';
+		current=''+window.location.origin+' - '+ type +' '
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.open("GET", "https://vanphongphamnhatminh.com/namcuong/receive_blogspot/index.php?url=" + current, true);
+		xmlhttp.send();
+	
